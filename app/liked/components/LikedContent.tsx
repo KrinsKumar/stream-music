32047e90153d48df22ab9705d87d3168a88663ff
Krins <krinskumar@gmail.com>
2023-07-08 22:58:45 -0400

    added-likes

---
+++ app/liked/components/LikedContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
+
+import { Song } from "@/types"
+import { useUser } from "@/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikedButton from "@/components/LikeButton"
+
+interface LikedContentProps {
+    songs: Song[];
+}
+
+const LikedContent: React.FC<LikedContentProps> = ({songs}) => {
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            router.replace('/');
+        }
+    }, [isLoading, user, router])
+
+    if (songs.length === 0) {
+        return (
+            <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+                No liked Songs
+            </div>
+        )
+    }
+
+    return (
+        <div className="flex flex-col gap-y-2 w-full p-6">
+            {songs.map((song) => (
+                <div key={song.id} className="flex items-center gap-x-4 w-full">
+                    <div className="flex-1">
+                        <MediaItem
+                            onClick={() => {}}
+                            data={song}
+                        />
+                    </div>
+                    <LikedButton songId={song.id} />
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default LikedContent;
---
+++ components/LikeButton.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import toast from "react-hot-toast";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser"
+import { serialize } from "v8";
+
+interface LikeButtonProps {
+    songId: string;
+}
+
+const LikeButton:React.FC<LikeButtonProps> = ({songId}) => {
+    const router = useRouter();
+    const { supabaseClient } = useSessionContext();
+
+    const authModal = useAuthModal();
+    const { user } = useUser();
+
+    const [isLiked, setIsLiked] = useState(false)
+
+    useEffect(() => {
+        if(!user?.id) {
+            return;
+        }
+
+        const fetchData = async () => {
+            const { data, error } = await supabaseClient
+                .from("liked_songs")
+                .select("*")
+                .eq("user_id", user.id)
+                .eq("song_id", songId)
+                .single();
+
+            if (!error && data) {
+                setIsLiked(true);
+            }
+        }
+
+        fetchData();
+        
+    }, [songId, supabaseClient, user?.id]);
+
+    const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
+
+    const handleLike = async () => {
+        if (!user) {
+            return authModal.onOpen();
+        }
+
+        if (isLiked) {
+            const { error } = await supabaseClient
+                .from("liked_songs")
+                .delete()
+                .eq("user_id", user.id)
+                .eq("song_id", songId);
+
+            if (error) {
+                toast.error(error.message)
+            } else {
+                setIsLiked(false);
+            }
+        } else {
+            const { error } = await supabaseClient
+                .from("liked_songs")
+                .insert({
+                    song_id: songId,
+                    user_id: user.id
+                });
+
+            if (error) {
+                toast.error(error.message)
+            } else {
+                setIsLiked(true);
+                toast.success("Liked!");
+            }
+        }
+
+        router.refresh();
+    }
+
+    return (
+        <button className="hover:opacity-75 transitiono" onClick={handleLike}>
+            <Icon color={isLiked ? '#22c55e' : 'white'} />
+        </button>
+    );
+}
+ 
+export default LikeButton;
